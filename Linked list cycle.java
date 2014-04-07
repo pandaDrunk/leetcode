f6d70b4235e1d42cd4a057a82e063e1be120d413
pandaDrunk <282002237@qq.com>
2014-04-07 19:00:10 -0400

    34.7%
    while condition: two.next!=null

---
+++ Linked list cycle.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode one=head;
+        ListNode two=head;
+        if(head==null) return false;
+        while(two!=null&&two.next!=null){
+            one=one.next;
+            two=two.next.next;
+            //if(two==null) return false;
+            if(one==two) return true;
+        }
+        return false;
+    }
+}
