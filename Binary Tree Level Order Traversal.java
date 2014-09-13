a409f2791f5dca46701b5b4f398d4ea7f0893877
pandaDrunk <282002237@qq.com>
2014-09-13 17:51:13 -0400

    30.9%

---
+++ Binary Tree Level Order Traversal.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> res=new ArrayList<ArrayList<Integer>>();
+        if(root==null) return res;
+        
+        //Stack<ArrayList<Integer>> stack=new Stack<ArrayList<Integer>>();
+        ArrayList<TreeNode> nodes=new ArrayList<TreeNode>();
+        nodes.add(root);
+        while(!nodes.isEmpty()){
+            ArrayList<TreeNode> temp=new ArrayList<TreeNode>();
+            ArrayList<Integer> level=new ArrayList<Integer>();
+            while(!nodes.isEmpty()){
+                TreeNode node=nodes.remove(0);
+                level.add(node.val);
+                if(node.left!=null) temp.add(node.left);
+                if(node.right!=null) temp.add(node.right);
+            }
+            res.add(level);
+            nodes=temp;
+        }
+        //while(!stack.isEmpty()){
+         //   res.add(stack.pop());
+        //}
+        return res;
+    }
+}
