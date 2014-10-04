6aaa33d41ad500f5585d040f7ec6e39539480927
pandaDrunk <282002237@qq.com>
2014-10-04 16:56:13 -0400

    27.9%

---
+++ Unique Path II.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        int[][] grid=new int[m][n];
+        for(int i=0;i<n;i++){
+            if(obstacleGrid[0][i]==0){
+                grid[0][i]=1;
+            }
+            else if(obstacleGrid[0][i]==1){
+                grid[0][i]=0;
+                break;
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(obstacleGrid[i][0]==0){
+                grid[i][0]=1;
+            }
+            else if(obstacleGrid[i][0]==1){
+                grid[i][0]=0;
+                break;
+            }
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(obstacleGrid[i][j]==0){
+                    grid[i][j]=grid[i-1][j]+grid[i][j-1];
+                }
+            }
+        }
+        return grid[m-1][n-1];
+    }
+}//same state transfer equation with Unique Path, just add a condition of abstacle grid
