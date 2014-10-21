884f6ddb8c0cc7fbb1ed15d7710ceff03f40c06e
pandaDrunk <282002237@qq.com>
2014-10-21 17:01:32 -0400

    31.4%

--- Find Minimum in Rotated Sorted Array.java
+++ Find Minimum in Rotated Sorted Array.java
@@ -8,7 +8,7 @@ public class Solution {
         if(num[m]<num[n]) return num[m];
         //if(m+1==n) return num[n];
         int mid = (m+n)/2;
-        
+        //the condition is simple
         if(num[m]>num[mid]){
             return findMin(num, m, mid);
         } else if(num[m]<num[mid]){
